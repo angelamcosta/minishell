da3c8f04ed62883bf2471f88fd42da1f6bba5539
Miguel Diogo <miguel.g.diogo@gmail.com>
2023-10-25 14:41:14 +0100

    🐛 fix `heredoc` signals
    Co-authored-by: Angela Lima <angelamcostalima@icloud.com>

--- src/builtins/heredoc.c
+++ src/builtins/heredoc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   heredoc.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
+/*   By: mpedroso <mpedroso@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 13:58:32 by anlima            #+#    #+#             */
-/*   Updated: 2023/10/15 13:48:05 by anlima           ###   ########.fr       */
+/*   Updated: 2023/10/25 14:36:27 by mpedroso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,25 @@ void	heredoc(char *delimiter);
 void	heredoc(char *delimiter)
 {
 	int		fd;
-	int		bytes_read;
-	char	line[1024];
+	char	*line;
 
+	signal(SIGINT, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
 	fd = open("temp_here.txt", O_WRONLY | O_CREAT | O_APPEND, 0777);
 	while (1)
 	{
-		write(1, "> ", 2);
-		bytes_read = read(STDIN_FILENO, &line, BUFF_SIZE);
+		line = readline("> ");
+		if (line == NULL)
+		{
+			write(0, "heredoc delimited by eof\n", 26);
+			break ;
+		}
 		if (ft_strncmp(delimiter, line, ft_strlen(delimiter)) == 0)
 			break ;
-		write(fd, line, bytes_read);
+		write(fd, line, ft_strlen(line));
+		free(line);
 	}
+	signals_child();
 }
 
 void	handle_heredocs(void)
